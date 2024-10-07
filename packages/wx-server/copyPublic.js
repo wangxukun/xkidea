c8aab6e01caaaec0db2abb129a2931a9e8a8da77
wxk <wxkzd-qq.com>
2024-10-07 15:20:05 +0800

    ci(copypublic.js): modify copyPublic.js, change apps to packages

--- packages/wx-server/copyPublic.js
+++ packages/wx-server/copyPublic.js
@@ -1,7 +1,10 @@
 const fs = require('fs-extra');
 async function copyPublic() {
   try {
-    await fs.copy('./apps/wx-server/public', './dist/apps/wx-server/public'); // 假设输出目录是 dist
+    await fs.copy(
+      './packages/wx-server/public',
+      './dist/packages/wx-server/public'
+    ); // 假设输出目录是 dist
     console.log('Public directory copied successfully!');
   } catch (err) {
     console.error('Error copying public directory:', err);
