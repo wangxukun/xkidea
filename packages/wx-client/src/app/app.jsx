bb7778349586e39db14b0edb5acebe74b513e8b1
wxk <wxkzd-qq.com>
2024-10-14 00:09:45 +0800

    feat(wx-client): add client app: wx-client

---
+++ packages/wx-client/src/app/app.jsx
@@ -0,0 +1,7 @@
+import 'weui';
+import { Button } from 'react-weui';
+import 'react-weui/build/packages/react-weui.css';
+export function App() {
+  return <Button>Hello wechat</Button>;
+}
+export default App;
