d858c02dd72a6e27eb93fbce19876b9bcc811678
wxk <wxkzd-qq.com>
2024-11-19 23:28:11 +0800

    refactor(wx-server/material.js wzl.js): 完善微信自动回复消息

--- packages/wx-server/src/api/wechat/wzl.js
+++ packages/wx-server/src/api/wechat/wzl.js
@@ -4,74 +4,95 @@ import { Material } from '../../utils/wechat/material';
 
 async function wzl(ctx) {
   try {
-    const jsonData = ctx.request.body; // POST方式获取的数据,JSON串
-    console.log('Handle Post jsonData is ', jsonData);
+    const jsonData = validateInput(ctx.request.body); // 验证输入
 
+    const appId = process.env.EPS_GHZ_APP_ID;
+    const appSecret = process.env.EPS_GHZ_APP_SECRET;
+    validateEnvironmentVariables(appId, appSecret); // 验证环境变量
+
+    // 获取素材列表
     const options = {
-      appId: process.env.EPS_GZH_APP_ID,
-      appSecret: process.env.EPS_GZH_APP_SECRET,
+      appId,
+      appSecret,
       type: 'image',
       offset: 0,
       count: 20,
     };
+    const material = new Material();
+    const { data } = await material.getMaterialList(options);
 
-    const { data } = await new Material().getMaterialList(options);
-    console.log('data', data);
-
+    // 解析消息
     const recMsg = await parseJson(jsonData);
     if (recMsg && recMsg.MsgType === 'text') {
       const toUser = recMsg.FromUserName;
       const fromUser = recMsg.ToUserName;
-
-      let replyMsg;
-      switch (recMsg.Content) {
-        case '雅思':
-          replyMsg = new ImageMsg(
-            toUser,
-            fromUser,
-            'S05-G8-3WTrOHeVHi2Pvucc-JRbjal3wkTxmmoiTX0zD8jb0wBCIaWZBkGruoqPD'
-          );
-          ctx.body = replyMsg.send();
-          break;
-        case '高中':
-          replyMsg = new ImageMsg(
-            toUser,
-            fromUser,
-            'S05-G8-3WTrOHeVHi2PvubdeZZ4cOPFyn3-F-5nckR4EB7AjcCnmwQKCnoZGSSfu'
-          );
-          ctx.body = replyMsg.send();
-          break;
-        case '四级':
-          replyMsg = new ImageMsg(
-            toUser,
-            fromUser,
-            'S05-G8-3WTrOHeVHi2PvuYJ3cchx1NvVInOuDq5d418g61V3rvLF7XdpRdhaU_Nd'
-          );
-          ctx.body = replyMsg.send();
-          break;
-        case '六级':
-          replyMsg = new ImageMsg(
-            toUser,
-            fromUser,
-            'S05-G8-3WTrOHeVHi2PvuZVgtGF23JArPcakqOGVLUtkiqaT9Sd3KOx4bXC9e1HJ'
-          );
-          ctx.body = replyMsg.send();
-          break;
-        default:
-          const content =
-            '感谢您的关注！请输入雅思，高中，四级或六级获取对应英语听力训练视频。';
-          replyMsg = new TextMsg(toUser, fromUser, content);
-          ctx.body = replyMsg.send();
-          break;
+      // 查找素材
+      const media_id = findMediaId(material, data, recMsg.Content);
+      // 判断是否找到素材
+      if (!media_id) {
+        const content =
+          '感谢您的关注！请输入雅思，高中，四级或六级获取对应英语听力训练视频。';
+        const replyMsg = new TextMsg(toUser, fromUser, content);
+        ctx.body = replyMsg.send();
+      } else {
+        const replyMsg = new ImageMsg(toUser, fromUser, media_id);
+        ctx.body = replyMsg.send();
       }
     } else {
       console.log('暂且不处理');
       ctx.body = 'success';
     }
   } catch (error) {
-    console.error(error);
-    ctx.body = error.message;
+    console.error(`Error in wzl function: ${error.start}`);
+    if (error instanceof SyntaxError) {
+      ctx.body = 'Invalid JSON data';
+    } else if (
+      error.message.includes('Missing required environment variables')
+    ) {
+      ctx.body = 'Internal server error';
+    } else {
+      ctx.body = error.message;
+    }
   }
+}
+
+/**
+ * 验证输入
+ * @param input
+ * @returns {*}
+ */
+function validateInput(input) {
+  if (typeof input !== 'object' || input === null) {
+    throw new Error('Invalid JSON data');
+  }
+  return input;
+}
+
+/**
+ * 验证环境变量
+ * @param appId
+ * @param appSecret
+ */
+function validateEnvironmentVariables(appId, appSecret) {
+  if (!appId || !appSecret) {
+    throw new Error('Missing required environment variables');
+  }
+}
+
+/**
+ * 查找素材
+ * @param material
+ * @param data
+ * @param content
+ * @returns {*}
+ */
+function findMediaId(material, data, content) {
+  return material.findMediaIdByName(data, `${content}.png`, [
+    '雅思.png',
+    '高中.png',
+    '四级.png',
+    '六级.png',
+  ]);
 }
 
 module.exports = {
--- packages/wx-server/src/utils/wechat/material.js
+++ packages/wx-server/src/utils/wechat/material.js
@@ -4,6 +4,11 @@ import { getAccess_token } from './sign';
 class Material {
   constructor() {}
 
+  /**
+   * Get the material list.
+   * @param options
+   * @returns {Promise<axios.AxiosResponse<any>>}
+   */
   async getMaterialList(options) {
     const { type, offset, count } = options;
 
@@ -13,6 +18,26 @@ class Material {
     );
     const materialUrl = `https://api.weixin.qq.com/cgi-bin/material/batchget_material?access_token=${access_token}`;
     return axios.post(materialUrl, { type, offset, count });
+  }
+
+  /**
+   * Find the media_id by name.
+   * @param materialList
+   * @param name
+   * @param range array
+   * @returns {*}
+   */
+  findMediaIdByName(materialList, name, range) {
+    // 检查 data 和 data.item 是否存在且为数组
+    if (!materialList || !Array.isArray(materialList.item)) {
+      return null;
+    }
+    // 检查 name 是否存在于 range 数组中
+    if (!range.includes(name)) return null;
+    // 遍历 materialList.item，找到与 name 相等的项
+    const item = materialList.item.find((item) => item.name === name);
+    // 返回 media_id
+    return item ? item.media_id : null;
   }
 }
 
