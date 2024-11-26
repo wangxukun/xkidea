d8572618d434813dac5380ed4095261760f24040
wxk <wxkzd-qq.com>
2024-11-27 00:11:14 +0800

    fix(/routes/index.js): 处理语音评测，取消插入模拟数据

--- packages/wx-server/src/api/wxapp/insertMockQuestions.js
+++ packages/wx-server/src/api/wxapp/insertMockQuestions.js
@@ -6,31 +6,31 @@ export default async function insertMockQuestions() {
   if (count === 0) {
     const questions = [
       {
-        topic: 'Greetings',
+        topic: '超实用英语句型',
         englishText: 'Have a good day',
         chineseText: '祝你有美好的一天',
         audioUrl: 'https://www.wxkzd.com/audios/audio1.mp3',
       },
       {
-        topic: 'Greetings',
+        topic: '超实用英语句型',
         englishText: 'How are you doing?',
         chineseText: '你好吗？',
         audioUrl: 'https://www.wxkzd.com/audios/audio1.mp3',
       },
       {
-        topic: 'Greetings',
+        topic: '会议英语',
         englishText: 'This is a practice sentence',
         chineseText: '这个是练习句',
         audioUrl: 'https://www.wxkzd.com/audios/audio1.mp3',
       },
       {
-        topic: 'Second',
+        topic: '超实用英语句型',
         englishText: 'Can you repeat that?',
         chineseText: '你能重复一遍吗？',
         audioUrl: 'https://www.wxkzd.com/audios/audio1.mp3',
       },
       {
-        topic: 'Second',
+        topic: '会议英语',
         englishText: 'What is your name?',
         chineseText: '你的名字是什么？',
         audioUrl: 'https://www.wxkzd.com/audios/audio1.mp3',
